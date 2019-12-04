c6472d999077ba878a7436c3c984b191cabe0915
Victor Siqueira da Cunha <codeakio@Mac-mini-de-Victor.local>
2019-12-03 23:35:04 -0300

    Email ou telefone opcionais

--- subscriptions/tests/test_view_new.py
+++ subscriptions/tests/test_view_new.py
@@ -81,3 +81,11 @@ class SubscriptionNewInvalid(TestCase):
 
     def test_dont_save_subscription(self):
         self.assertFalse(Subscription.objects.exists())
+
+
+class TemplateRegressionTest(TestCase):
+    def test_template_has_non_field_errors(self):
+        invalid_data = dict(name='Victor', cpf='11122233344')
+        response = self.client.post(resolve_url('subscriptions:new'), invalid_data)
+
+        self.assertContains(response, '<ul class="errorlist nonfield">')
